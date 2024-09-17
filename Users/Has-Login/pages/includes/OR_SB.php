1abbb1e1113c9fd0c53e833de9787755b15ebdd7
aalllddiii <aldicahyar@gmail.com>
2024-09-17 10:25:34 +0700

    first commit

---
+++ Users/Has-Login/pages/includes/OR_SB.php
@@ -0,0 +1,79 @@
+<div class="card mb-2">
+    <div class="card-header" style="padding: 0px;">
+        <h5 class="mb-0">
+            <a class="btn" data-toggle="collapse" href="#BidangORSB" role="button" aria-expanded="true"
+                aria-controls="BidangORSB" style="color: #723d90;">Bidang Minat dan Bakat</a>
+        </h5>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="collapse multi-collapse" id="BidangORSB">
+                <div class="card card-body bidang-mk">
+                    <span class="mb-3">
+                    Memiliki satu kepala bidang dan membawahi dua departemen, yaitu:
+
+                    </span>
+                    <!-- Departemen ORSB -->
+                    <div class="card mb-2">
+                        <div class="card-header" id="DeptOR" style="padding: 0px;">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link" data-toggle="collapse" data-target="#OR"
+                                    aria-expanded="true" aria-controls="OR" style="color: #723d90;">
+                                    Departemen Olahraga dan Seni Budaya
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="OR" class="collapse" aria-labelledby="DeptOR" data-parent="#accordion">
+        <div class="row text-center">
+            <div class="col-md-6 col-6 text-center">
+                <img src="../../../assets/images/Olahraga.png" alt="" class="images-desc-logo">
+                <p class="capt-logo-au">Olahraga</p>
+            </div>
+            <div class="col-md-6 col-6 text-center">
+                <img src="../../../assets/images/Seni Budaya.png" alt="" class="images-desc-logo">
+                <p class="capt-logo-au">Seni Budaya</p>
+            </div>
+            <div class="col-md-12 col-12 deskripsi-dept-biro">
+            Departemen Olahraga dan Seni Budaya merupakan departemen yang berperan aktif dalam mewadahi, memfasilitasi, mengembangkan, dan menyalurkan minat bakat mahasiswa serta menumbuhkan jiwa sportivitas dan kreativitas mahasiswa FIKTI UG dalam bidang olahraga dan kesenian
+                <hr class="col-10">
+                <span class="text-muted" style="font-style: italic; font-size: 10pt !important;">
+                Benefit: Menambah pengalaman dalam mengurus kepanitiaan acara bakat
+                                        minat olahraga dan seni budaya yang ada di FIKTI.
+                </span>
+            </div>
+        </div>
+    </div>
+                    </div>
+                    <div class="card mb-2">
+                        <div class="card-header" id="DeptAkademik" style="padding: 0px;">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link" data-toggle="collapse" data-target="#Akademik"
+                                    aria-expanded="true" aria-controls="Akademik" style="color: #723d90;">
+                                    Departemen Akademik
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="Akademik" class="collapse" aria-labelledby="DeptAkademik" data-parent="#accordion">
+                            <div class="row">
+                                <div class="col-md-3 text-center">
+                                    <img src="../../../assets/images/Akademik.png" alt="" class="images-desc-logo">
+                                </div>
+                                <div class="col-md-8 card-body deskripsi-dept-biro">
+                                Departemen Akademik berfungsi sebagai wadah bagi mahasiswa FIKTI UG dalam mengembangkan, mendukung dan memfasilitasi minat bakat di bidang Akademik serta eksplorasi pengembangan karir.
+                                    <hr class="col-10">
+                                    <span class="text-muted" style="font-style: italic;">
+                                        Benefit: Menambah pengalaman dalam membentuk serta mengembangkan acara bidang
+                                        akademis seperti seminar, workshop, dan kompetisi. Menambah pengalaman dalam
+                                        berkomunikasi dengan pihak sponsor yang nantinya bisa menjadi sponsor dari
+                                        proker yang dipimpin. Turut serta mengembangkan potensi mahasiswa FIKTI dalam
+                                        bidang akademis.
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ Users/Has-Login/pages/includes/Polkesma.php
@@ -0,0 +1,76 @@
+<div class="card mb-2">
+    <div class="card-header" id="BidangPolKesma" style="padding: 0px;">
+        <h5 class="mb-0">
+            <button class="btn btn-link" data-toggle="collapse"  href="#BidangPolkesma" role="button" aria-expanded="true"
+                aria-controls="AU" style="color: #723d90;">
+                Bidang Sosial dan Politik
+            </button>
+        </h5>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="collapse multi-collapse" id="BidangPolkesma">
+                <div class="card card-body bidang-mk">
+                    <span class="mb-3">
+                    Memiliki satu kepala bidang dan membawahi dua departemen, yaitu:
+                    </span>
+                    <!-- Departemen Akademik -->
+                    <div class="card mb-2">
+                        <div class="card-header" id="BidangPolKesma" style="padding: 0px;">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link" data-toggle="collapse" data-target="#PolKesma"
+                                    aria-expanded="true" aria-controls="PolKesma" style="color: #723d90;">
+                                    Departemen Politik dan Kesejahteraan Mahasiswa
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="PolKesma" class="collapse" aria-labelledby="BidangPolKesma" data-parent="#accordion">
+                            <div class="row">
+                                <div class="col-md-3 text-center">
+                                    <img src="../../../assets/images/Kastrat.png" alt="" class="images-desc-logo">
+                                </div>
+                                <div class="col-md-8 card-body deskripsi-dept-biro">
+                                Departemen Politik dan Kesejahteraan Mahasiswa (POLKESMA) adalah departemen yang bertugas sebagai penghubung dan pelayanan kebutuhan terhadap mahasiswa FIKTI dengan pemangku kebijakan, 
+                                pengadvokasian kebijakan internal kampus terhadap isu-isu yang berkembang, 
+                                serta roda penggerak dalam pergerakan mahasiswa FIKTI.
+                                    <hr class="col-10">
+                                    <span class="text-muted" style="font-style: italic;">
+                                    Menambah relasi antar mahasiswa FIKTI UG baik jurusan Sistem Informasi dan Sistem Komputer, 
+                                    ikut serta dalam membangun FIKTI UG yang lebih baik lagi ke depannya, 
+                                    dan mensejahterakan mahasiswa FIKTI UG.
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Departemen Sosial Masyarakat -->
+                    <div class="card mb-2">
+                        <div class="card-header" id="DeptPengMas" style="padding: 0px;">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link" data-toggle="collapse" data-target="#Pengmas"
+                                    aria-expanded="true" aria-controls="Pengmas" style="color: #723d90;">
+                                    Departemen Sosial Masyarakat
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="Pengmas" class="collapse" aria-labelledby="DeptPengMas" data-parent="#accordion">
+                            <div class="row">
+                                <div class="col-md-3 text-center">
+                                    <img src="../../../assets/images/Pengmas.png" alt="" class="images-desc-logo">
+                                </div>
+                                <div class="col-md-8 card-body deskripsi-dept-biro">
+                                Departemen Sosial Masyarakat merupakan satu departemen BEM FIKTI UG yang bertugas dan bertanggung jawab sebagai wadah dan kolaborator kegiatan dan isu sosial dalam masyarakat, 
+                                baik internal maupun eksternal kampus untuk saling menebar kebermanfaatan
+                                    <hr class="col-10">
+                                    <span class="text-muted" style="font-style: italic;">
+                                    Benefit: Berperan sebagai roda penggerak dalam pergerakan mahasiswa FIKTI.
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
